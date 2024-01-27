dc81581e8921f34378657e188f35940b235b3eac
prajwalathreya <athreya.p@northeastern.edu>
2024-01-27 14:17:43 -0500

    matrix update

--- resizeimage.py
+++ resizeimage.py
@@ -1,7 +1,7 @@
 import cv2
 
-img = cv2.imread('/home/prajwal/Documents/projects/prajwal_athreya/images/')
+img = cv2.imread(r'./images/detecto.png')
 
-image = cv2.resize(img,(4000,4000),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+image = cv2.resize(img,(580,310),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
 
-cv2.imwrite('./images/',image)
+cv2.imwrite('./images/rns.png',image)
